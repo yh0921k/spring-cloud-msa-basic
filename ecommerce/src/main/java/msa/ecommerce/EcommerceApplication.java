bcce532904db9e2f3b94cf321e150ad372e491b5
Yonghwi Kim <yh0921k@gmail.com>
2022-11-27 17:23:45 +0900

    chore(DiscoveryService): Service Discovery 기본 설정
    EcommerceApplication.java
    - @EnableEurekaServer 어노테이션을 명시하여 서비스 디스커버리 역할 수행
    
    application.yml
    - 서비스 포트 설정
    - 마이크로 서비스 고유 ID 설정
    - 특히, eureka.client 하위 옵션은 설정하지 않으면 본 서비스를 Service Discovery에 등록하는 형태가됨
      > 현재 클라이언트가 아닌, 서버 설정이므로 이를 false로 설정(자기 자신을 등록하지 않음)

--- ecommerce/src/main/java/msa/ecommerce/EcommerceApplication.java
+++ ecommerce/src/main/java/msa/ecommerce/EcommerceApplication.java
@@ -2,12 +2,13 @@ package msa.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EcommerceApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(EcommerceApplication.class, args);
   }
-
 }
