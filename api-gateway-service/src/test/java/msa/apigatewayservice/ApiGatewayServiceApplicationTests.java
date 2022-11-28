1a7b7bfb4a21cfd45ff721cd268245e150aeba8a
Yonghwi Kim <yh0921k@gmail.com>
2022-11-28 13:02:41 +0900

    init(APIGatewayService): 스프링 클라우드를 이용한 API Gateway 프로젝트 초기화
    - 기존과 다르게 Netty라는 비동기 내장 서버가 동작함(Tomcat이 아님)
    - Zuul에서는 동기방식 서버를 사용했으나, Spring Cloud API Gateway 서비스를 실행하면 비동기 방식의 서버를 사용한다.
    
    application.yml
    - 현재 Eureka 관련 설정은 추후 사용 예정
    - Path에 명시한 경로가 해당 서비스에 그대로 전달됨에 주의
      > 차후 필터를 적용해 사용자 요청 정보를 변환하는 작업(Header, QueryString, RequestURI)으로 처리할 수 있음

---
+++ api-gateway-service/src/test/java/msa/apigatewayservice/ApiGatewayServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package msa.apigatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayServiceApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
