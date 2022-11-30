89f5da508a2facbe4614472346bd07c29812619f
Yonghwi Kim <yh0921k@gmail.com>
2022-11-30 23:16:01 +0900

    feat(UserService): DTO, VO 클래스 구현
    RequestUser.java
    ResponseUser.java
    - 사용자 요청 및 응답 객체로 사용될 클래스 구현
    
    UserDto.java
    - 컨트롤러, 서비스 레이어간 데이터 전송 형식 클래스 구현

---
+++ user-service/src/main/java/msa/userservice/dto/UserDto.java
@@ -0,0 +1,15 @@
+package msa.userservice.dto;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class UserDto {
+  private String email;
+  private String name;
+  private String password;
+  private String userId;
+  private LocalDateTime createdAt;
+  private String encryptedPassword;
+}
---
+++ user-service/src/main/java/msa/userservice/vo/ResponseUser.java
@@ -0,0 +1,10 @@
+package msa.userservice.vo;
+
+import lombok.Data;
+
+@Data
+public class ResponseUser {
+  private String email;
+  private String name;
+  private String userId;
+}
