117fad5c31a4f1c977a559b40b8c82449b6207c6
Yonghwi Kim <yh0921k@gmail.com>
2022-12-01 11:46:10 +0900

    feat(UserService): 조회 기능을 위한 DTO, VO 추가 및 수정
    ResponseOrder.java
    ResponseUser.java
    - @JsonInclude 어노테이션은 Null인 필드를 제외
    
    UserDto.java
    - 사용자 주문 정보 필드 추가

--- user-service/src/main/java/msa/userservice/dto/UserDto.java
+++ user-service/src/main/java/msa/userservice/dto/UserDto.java
@@ -1,8 +1,10 @@
 package msa.userservice.dto;
 
 import lombok.Data;
+import msa.userservice.vo.ResponseOrder;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
 public class UserDto {
@@ -12,4 +14,6 @@ public class UserDto {
   private String userId;
   private LocalDateTime createdAt;
   private String encryptedPassword;
+
+  private List<ResponseOrder> orders;
 }
