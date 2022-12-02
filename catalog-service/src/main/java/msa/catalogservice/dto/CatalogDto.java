ea7d890b14f758d627e9ccaed07806ba1ab99bed
Yonghwi Kim <yh0921k@gmail.com>
2022-12-02 18:38:43 +0900

    feat(CatalogService): VO, DTO 구현
    CatalogDto.java
    - 레이어간 데이터 전송 형식 클래스 구현
    
    ResponseCatalog.java
    - API 응답 형식 클래스 구현
    - Null인 필드를 반환에 포함하지 않음

---
+++ catalog-service/src/main/java/msa/catalogservice/dto/CatalogDto.java
@@ -0,0 +1,15 @@
+package msa.catalogservice.dto;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class CatalogDto implements Serializable {
+  private String productId;
+  private Integer quantity;
+  private Integer unitPrice;
+  private Integer totalPrice;
+  private String orderId;
+  private String userId;
+}
