1a9fc8b9bf4521e3afb44d6ace0c7e2424fbca35
Yonghwi Kim <yh0921k@gmail.com>
2022-12-05 17:34:57 +0900

    feat(APIGatewayService): Spring Actuator를 이용한 설정 정보 갱신
    - Local Git Config Repository의 설정 정보를 업데이트하고 커밋하면 사용자 서비스 뿐만 아니라 API Gateway 서비스도 설정 정보를 갱신해야함
    - Spring Actuator를 이용하여 ConfigServer의 업데이트를 갱신할 수 있음
    
    build.gradle
    - Config 관련 의존성 추가
    - Spring Actuator 의존성 추가
    - Actuator는 운영 중인 애플리케이션을 HTTP나 JMX를 이용해서 모니터링하고 관리할 수 있는 기능을 제공
    
    bootstrap.yml
    - SpringBoot 2.4.0 이후 버전에서 부트스트랩 정보를 자동으로 읽지 않아 의존성 및 설정 추가 필요
    
    application.yml
    - 사용자 서비스에 대한 routes 설정 추가
    - refresh, health, beans, httptrace 엔드포인트 지정
    - 특히 refresh는 서버 재기동 없이 설정 정보를 갱신하는데 사용
    
    ApiGatewayServiceApplication.java
    - Actuator의 httptrace 엔드포인트 사용을 위한 빈 등록

--- api-gateway-service/src/main/java/msa/apigatewayservice/ApiGatewayServiceApplication.java
+++ api-gateway-service/src/main/java/msa/apigatewayservice/ApiGatewayServiceApplication.java
@@ -1,12 +1,20 @@
 package msa.apigatewayservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiGatewayServiceApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(ApiGatewayServiceApplication.class, args);
+  }
+
+  @Bean
+  public HttpTraceRepository httpTraceRepository() {
+    return new InMemoryHttpTraceRepository();
   }
 }
