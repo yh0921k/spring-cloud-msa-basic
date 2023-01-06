23bc33176f93ad7c44e94ba9e6a7a7d9df32067b
Yonghwi Kim <yh0921k@gmail.com>
2023-01-06 12:35:00 +0900

    chore(UserService): 배포 구성을 위한 버전 변경
    build.gradle
    - 버전 변경
    
    Dockerfile
    build.sh
    - 버전 변경에 따른 JAR 파일명 변경

--- docker-files/user-service/build.sh
+++ docker-files/user-service/build.sh
@@ -2,6 +2,6 @@
 
 cd ../../user-service/
 ./gradlew clean bootJar
-cp build/libs/user-service-0.0.1-SNAPSHOT.jar ../docker-files/user-service/
+cp build/libs/user-service-1.0.jar ../docker-files/user-service/
 cd ../docker-files/user-service/
 docker build -t yh0921k/user-service:1.0 .
